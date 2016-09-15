d1a8c8ef701afda43356cd562bf3f181a7386f8e
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2016-09-15 21:34:18 +1000

    initial commit of flynn-ghost

---
+++ config.js
@@ -0,0 +1,56 @@
+// Ghost Configuration for Heroku
+
+var path = require('path'),
+    config,
+    fileStorage,
+    storage;
+
+config = {
+
+  // Production (Heroku)
+  production: {
+    url: process.env.APP_URL,
+    mail: {
+      transport: 'SMTP',
+      options: {
+        service: 'Mailgun',
+        auth: {
+          user: process.env.MAILGUN_SMTP_LOGIN,
+          pass: process.env.MAILGUN_SMTP_PASSWORD
+        }
+      }
+    },
+    database: {
+      client: 'mysql',
+      connection: process.env.DATABASE_URL,
+      debug: false
+    },
+    server: {
+      host: '0.0.0.0',
+      port: process.env.PORT
+    },
+  },
+
+  // Development
+  development: {
+    url: 'http://localhost:2368',
+    database: {
+      client: 'sqlite3',
+      connection: {
+        filename: path.join(__dirname, '/content/data/ghost-dev.db')
+      },
+      debug: false
+    },
+    server: {
+      host: '127.0.0.1',
+      port: '2368'
+    },
+    paths: {
+      contentPath: path.join(__dirname, '/content/')
+    }
+  }
+
+};
+
+// Export config
+module.exports = config;
